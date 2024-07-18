509a28f43e430ea91f276d0d2b4822019c7ada34
misha bansal <mishabansal26@gmail.com>
2024-07-18 18:34:22 +0530

    DONE

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $("#sgnup").click(function(){
+
+        alert();
+        // let obj={
+        //     type:"get",
+        //     url:"/signup-process",
+        //     data:{
+        //         txtEmail:$("#txtEmail").val(),
+        //         pwd:$("pwd").val(),
+
+        //}
+        
+    });
+});
